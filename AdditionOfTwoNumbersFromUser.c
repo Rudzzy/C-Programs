3c8ccb580fcda0810a88517d454b2cb2ac8f5545
rudzzy <rudrapriyansh4@gmail.com>
2024-02-24 11:54:14 +0530

    Addion of any two integers

---
+++ AdditionOfTwoNumbersFromUser.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1,n2;
+
+    printf("Enter Two Intergers: ");
+    scanf("%d %d",&n1,&n2);
+
+    printf("Output: %d + %d = %d",n1,n2,n1+n2);
+    
+    return 0;
+}
