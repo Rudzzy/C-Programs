528499fa5212806f797278c888bcea140389f9de
rudzzy <rudrapriyansh4@gmail.com>
2024-03-13 19:29:29 +0530

    Program to print Current time and date.

---
+++ CurrentTime.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    struct tm* ptr;
+    time_t t;
+    t = time(NULL);
+    ptr = localtime(&t);
+    printf("%s", asctime(ptr));
+
+    return 0;
+}
